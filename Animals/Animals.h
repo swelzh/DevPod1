c2429512d9991eb847c5be32d1430a4e24f45e23
swelzh <swelzh@gmail.com>
2019-01-08 01:54:49 +0800

    add   animal

---
+++ Animals/Animals.h
@@ -0,0 +1,16 @@
+//
+//  Animals.h
+//  DevPod1
+//
+//  Created by apple on 2019/1/8.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Animals : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
